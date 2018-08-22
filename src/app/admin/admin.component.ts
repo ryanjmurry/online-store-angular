d54864867b1c14bb59b8e433486a5640b8a19515
Student <example@example.com>
2018-08-22 13:20:03 -0700

    import album service and add method to add new album to the database

--- src/app/admin/admin.component.ts
+++ src/app/admin/admin.component.ts
@@ -1,15 +1,24 @@
 import { Component, OnInit } from '@angular/core';
+import { Album } from '../models/album.model';
+import { AlbumService } from '../album.service';
 
 @Component({
   selector: 'app-admin',
   templateUrl: './admin.component.html',
-  styleUrls: ['./admin.component.css']
+  styleUrls: ['./admin.component.css'],
+  providers: [AlbumService]
 })
 export class AdminComponent implements OnInit {
 
-  constructor() { }
+  constructor(private albumService: AlbumService) { }
 
   ngOnInit() {
+  }
+
+  submitForm(title: string, artist: string, description: string) {
+    let newAlbum = new Album(title, artist, description);
+    this.albumService.addAlbum(newAlbum);
+    console.log(newAlbum);
   }
 
 }
