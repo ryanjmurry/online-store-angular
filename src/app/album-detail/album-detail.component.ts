347f30f4b92da4d5ed35071735c58c7ca9b45a27
Student <example@example.com>
2018-08-22 10:57:37 -0700

    add album service and logic to get clicked album's details

--- src/app/album-detail/album-detail.component.ts
+++ src/app/album-detail/album-detail.component.ts
@@ -2,20 +2,27 @@ import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Params } from '@angular/router';
 import { Location } from '@angular/common';
 import { Album } from '../models/album.model';
+import { AlbumService } from '../album.service';
 
 @Component({
   selector: 'app-album-detail',
   templateUrl: './album-detail.component.html',
-  styleUrls: ['./album-detail.component.css']
+  styleUrls: ['./album-detail.component.css'],
+  providers: [AlbumService]
 })
 export class AlbumDetailComponent implements OnInit {
   albumId: number = null;
-  constructor(private route: ActivatedRoute, private location: Location) { }
+  albumToDisplay: Album;
+  constructor(
+    private route: ActivatedRoute,
+    private location: Location,
+    private albumService: AlbumService
+  ) {}
 
   ngOnInit() {
     this.route.params.forEach((urlParameters) => {
       this.albumId = parseInt(urlParameters['id']);
     });
+    this.albumToDisplay = this.albumService.getAlbumById(this.albumId);
   }
-
 }
